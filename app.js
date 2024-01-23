f78e420e53870bdaf457672c2d3b921b39cedc0e
Heisenberg <omaradelawad20013@gmail.com>
2024-01-23 10:40:32 +0200

    successfully connected to the database

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const connectDB = require("./db/connect");
+require("dotenv").config();
 
 const app = express();
 
@@ -10,4 +12,15 @@ app.use("/api/v1/tasks", tasksRouter);
 
 const port = 5000;
 
-app.listen(port, () => console.log(`Server is listening on port ${port}...`));
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
