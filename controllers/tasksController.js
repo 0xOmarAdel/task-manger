0a3038a43e8fed167284bc9c6289faf48b6c8a10
Heisenberg <omaradelawad20013@gmail.com>
2024-01-23 09:53:38 +0200

    created all the tasks routes

--- controllers/tasksController.js
+++ controllers/tasksController.js
@@ -1,5 +1,21 @@
+const createTask = (req, res) => {
+  res.send([]);
+};
+
 const getAllTasks = (req, res) => {
   res.send([]);
 };
 
-module.exports = { getAllTasks };
+const getTask = (req, res) => {
+  res.send([]);
+};
+
+const updateTask = (req, res) => {
+  res.send([]);
+};
+
+const deleteTask = (req, res) => {
+  res.send([]);
+};
+
+module.exports = { createTask, getAllTasks, getTask, updateTask, deleteTask };
--- routes/tasksRouter.js
+++ routes/tasksRouter.js
@@ -1,7 +1,18 @@
 const express = require("express");
-const { getAllTasks } = require("../controllers/tasksController");
+const {
+  createTask,
+  getAllTasks,
+  getTask,
+  updateTask,
+  deleteTask,
+} = require("../controllers/tasksController");
+
 const router = express.Router();
 
 router.route("/").get(getAllTasks);
+router.route("/").post(createTask);
+router.route("/:id").get(getTask);
+router.route("/:id").patch(updateTask);
+router.route("/:id").delete(deleteTask);
 
 module.exports = router;
