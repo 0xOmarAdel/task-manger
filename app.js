c07076c6f885d57e96c2dd4137a547825f732524
Heisenberg <omaradelawad20013@gmail.com>
2024-01-23 09:43:19 +0200

    express server set up

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log('Task Manager App')
+const express = require("express");
+
+const app = express();
+
+const port = 5000;
+
+app.listen(port, () => console.log(`Server is listening on port ${port}...`));
